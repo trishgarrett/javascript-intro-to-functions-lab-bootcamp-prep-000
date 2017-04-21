bae419130706ccc895bad6a6ded5661c9703419c
Trisha Garrett <trisha.garrett@gmail.com>
2017-04-21 17:36:15 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -3,12 +3,18 @@ describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO')
   })
+function shout(string){
+  return string.toUpperCase()
+}
 })
 
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello')
   })
+  function whisper(string){
+    return string.toLowerCase()
+  }
 })
 
 describe('logShout(string)', function() {
